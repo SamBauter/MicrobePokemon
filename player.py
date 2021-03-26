330b4abd676e60d0b79f02024870c0560bd96862
Sam <samuelbauter@gmail.com>
2021-03-26 15:57:30 -0600

    first commit

---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+import config
+
+
+class Player:
+    def __init__(self, x_pos, y_pos):
+        print("player created")
+        self.position = [x_pos, y_pos]
+        self.image = pygame.image.load("imgs/player_05.png")
+        self.image = pygame.transform.scale(self.image, (config.SCALE, config.SCALE))
+        self.rect = pygame.Rect(self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE)
+
+    def update(self):
+        print("player updated")
+
+    def update_position(self, x_change, y_change):
+        self.position[0]+=x_change
+        self.position[1]+=y_change
+        self.rect = pygame.Rect(self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE)
+
+
+    def render(self, screen):
+        screen.blit(self.image, self.rect)
+
